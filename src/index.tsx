06aa4654e913676fbf126f1ed73d228e1d4a9d14
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2024-04-15 14:05:45 +0300

    folder struct + cleanup

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "App";
+
+const root = ReactDOM.createRoot(document.querySelector("root")!);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+// TODO: Uncomment or delete
+// reportWebVitals();
