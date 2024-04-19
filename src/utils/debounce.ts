33d8244ff17b03266eb14cefb35a16e15a6c61eb
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2024-04-19 13:07:49 +0300

    move debounce, not a hook

---
+++ src/utils/debounce.ts
@@ -0,0 +1,9 @@
+const useDebounce = (fn: Function, ms = 300) => {
+  let debounce: ReturnType<typeof setTimeout>;
+  return function (this: any, ...args: any[]) {
+    clearTimeout(debounce);
+    debounce = setTimeout(() => fn.apply(this, args), ms);
+  };
+};
+
+export default useDebounce;
